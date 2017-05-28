36563920da0d06d76acff26e2a0bf021ec517db3
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2017-05-28 10:05:30 -0400

    Fixed fizzbuzz, circuit code formatting

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -5,8 +5,18 @@
 
 :- use_module(library(clpfd)).
 
-fizz(N) :- N in 1..100, 0 #= N mod 3.
-buzz(N) :- N in 1..100, 0 #= N mod 5.
-fizzbuzz(N) :- fizz(N), buzz(N).
+fizzbuzz(N) :- N in 1..100, 0 #= N mod 15.
+fizz(N) :- N in 1..100, 0 #= N mod 3, 0 #\= N mod 5.
+buzz(N) :- N in 1..100, 0 #= N mod 5, 0 #\= N mod 3.
+
+numToSymbol(N, fizzbuzz) :- fizzbuzz(N).
+numToSymbol(N, fizz) :- fizz(N).
+numToSymbol(N, buzz) :- buzz(N).
+numToSymbol(N, N) :- integer(N).
+
+fizzbuzzList([], []).
+fizzbuzzList([N|Nums], [Sym|Result]) :-
+    numToSymbol(N, Sym),
+    fizzbuzzList(Nums, Result).
 
 
